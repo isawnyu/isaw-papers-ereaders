842d02019f4976d5682c9d36bafee6a793f904f8
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-05-01 09:49:59 -0400

    adding a bash script

---
+++ calibre.sh
@@ -0,0 +1,8 @@
+for i in 8 9; do
+    for j in $i/images/*.png; do
+        printf "Resize $i\n"
+        convert "$j" -resize 640x480 "$j"
+    done
+    ebook-convert $i/isaw-papers-$i-ereaders.xhtml $i/isaw-papers-$i.epub --cover cover.png --preserve-cover-aspect-ratio 
+    ebook-convert $i/isaw-papers-$i.epub $i/isaw-papers-$i.mobi  --no-inline-toc
+done
