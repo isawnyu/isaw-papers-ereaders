42a802dc4f283ee01e712623e93dc4f8eb183b7f
Fanny Mezard <fanny@NACOLA01.ISAW.NYU.EDU>
2018-05-31 13:59:47 -0400

    launching the bash script get all the work done

--- cleaning_epub.py
+++ cleaning_epub.py
@@ -1,3 +1,9 @@
+########################################################################################
+
+### DO NOT LAUNCH THAT SCRIPT : IT IS AUTOMATICALLY LAUMCHED BY THE BAHS SCRIPT #######
+
+########################################################################################
+
 from bs4 import BeautifulSoup
 import re
 import os
@@ -82,12 +88,10 @@ def video_11() :
 	"""
 	with open('11/isaw-papers-11-ereaders.xhtml', 'r') as paper:
 		isawPaper = paper.read()
-		isawPaper = isawPaper.replace('../Video/','http://dlib.nyu.edu/awdl/isaw/isaw-papers/11/animations/')
-		isawPaper = isawPaper.replace('a src','a href')
+		isawPaper = isawPaper.replace('animations/','http://dlib.nyu.edu/awdl/isaw/isaw-papers/11/animations/')
 		isawPaper = isawPaper.replace('<video controls="yes" width="100%">','')
 		isawPaper = isawPaper.replace('</video>','')
 		isawPaper = isawPaper.replace('Your browser does not support the video tag.','')
-		isawPaper = isawPaper.replace('mp4"/>','mp4">Click here for the link</a>')
 		soup = BeautifulSoup(isawPaper)
 
 	sources_webm = soup.find_all("source", {"type":"video/webm"})
@@ -98,6 +102,9 @@ def video_11() :
 	for source in sources :
 		source.name="a"
 		del source["type"]
+		source["href"] = source["src"]
+		del source["src"]
+		source.append("Click here for the link")
 
 	soup.prettify()
 
@@ -106,14 +113,51 @@ def video_11() :
 
 
 # applying all of the changes
-video_11()
+
 for i in range(1, 14):
 	with open('isaw-papers/isaw-papers-'+str(i)+'/isaw-papers-'+str(i)+'.xhtml') as paper:
-		soup = BeautifulSoup(paper)
+
+		# manual change of all the links in ISAW Papers 7
+		text = paper.read()
+		text = text.replace("elliott-heath-muccigrosso/", "elliott-heath-muccigrosso/isaw-papers-7-1.xhtml")
+		text = text.replace("acheson/", "acheson/isaw-papers-7-2.xhtml")
+		text = text.replace("almas-babeu-krohn/", "almas-babeu-krohn/isaw-papers-7-3.xhtml")
+		text = text.replace("benefiel-sprenkle/", "benefiel-sprenkle//isaw-papers-7-4.xhtml")
+		text = text.replace("blackwell-smith/", "blackwell-smith//isaw-papers-7-5.xhtml")
+		text = text.replace("elliott-jones/", "elliott-jones/isaw-papers-7-6.xhtml")
+		text = text.replace("hafford/", "hafford/isaw-papers-7-7.xhtml")
+		text = text.replace("heath/", "heath/isaw-papers-7-8.xhtml")
+		text = text.replace("horne/", "horne/isaw-papers-7-9.xhtml")
+		text = text.replace("kansa/", "kansa/isaw-papers-7-10.xhtml")
+		text = text.replace("lana/", "lana/isaw-papers-7-11.xhtml")
+		text = text.replace("liuzzo/", "liuzzo/isaw-papers-7-12.xhtml")
+		text = text.replace("mackay/", "mackay/isaw-papers-7-13.xhtml")
+		text = text.replace("mcmichael/", "mcmichael/isaw-papers-7-14.xhtml")
+		text = text.replace("meadows-gruber/", "meadows-gruber/isaw-papers-7-15.xhtml")
+		text = text.replace("meyers/", "meyers/isaw-papers-7-16.xhtml")
+		text = text.replace("murray/", "murray/isaw-papers-7-17.xhtml")
+		text = text.replace("nurmikko-fuller/", "nurmikko-fuller/isaw-papers-7-18.xhtml")
+		text = text.replace("pearce-schmitz/", "pearce-schmitz/isaw-papers-7-19.xhtml")
+		text = text.replace("pett/", "pett/isaw-papers-7-20.xhtml")
+		text = text.replace("poehler/", "poehler/isaw-papers-7-21.xhtml")
+		text = text.replace("rabinowitz/", "rabinowitz/isaw-papers-7-22.xhtml")
+		text = text.replace("reinhard/", "reinhard/isaw-papers-7-23.xhtml")
+		text = text.replace("romanello/", "romanello/isaw-papers-7-24.xhtml")
+		text = text.replace("roueche-lawrence-lawrence/", "roueche-lawrence-lawrence/isaw-papers-7-25.xhtml")
+		text = text.replace("seifried/", "seifried/isaw-papers-7-26.xhtml")
+		text = text.replace("simon-barker-desoto-isaksen/", "simon-barker-desoto-isaksen/isaw-papers-7-27.xhtml")
+		text = text.replace("taylor/", "taylor/isaw-papers-7-28.xhtml")
+		text = text.replace("tsonev/", "tsonev/isaw-papers-7-29.xhtml")
+		text = text.replace("vankeer/", "vankeer/isaw-papers-7-30.xhtml")
+
+		soup = BeautifulSoup(text, "lxml")
 		soup = cleaning(soup)
-		copy_pictures() # can be commented out if the pictures are already copied in order to speed up the script
+		copy_pictures()
+
 	with open(str(i)+'/isaw-papers-'+str(i)+'-ereaders.xhtml', 'w') as paper:
 		paper.write(str(soup))
+		print("ISAW Papers" + str(i) + "is ready")
+video_11()
 
 
 	
@@ -127,46 +171,9 @@ for element in os.listdir('isaw-papers/isaw-papers-7'):
 		for el in os.listdir('isaw-papers/isaw-papers-7/'+ str(element)):
 			if re.match("isaw-papers-7-*", str(el)):
 				with open ('isaw-papers/isaw-papers-7/'+ str(element) + '/'+ str(el), "r") as paper : 
-					soup = BeautifulSoup(paper)
+					soup = BeautifulSoup(paper, "lxml")
 					soup = cleaning(soup)
 					el = el.replace(".xhtml", "-ereaders.xhtml")
 				with open('7/'+ str(element)+'/' + str(el), 'w') as paper:
 					paper.write(str(soup))
 
-# manual change of all the links in ISAW Papers 7
-with open('7/isaw-papers-7-ereaders.xhtml', 'r') as paper:
-	text = paper.read()
-	text = text.replace("elliott-heath-muccigrosso/", "elliott-heath-muccigrosso/isaw-papers-7-1.xhtml")
-	text = text.replace("acheson/", "acheson/isaw-papers-7-2.xhtml")
-	text = text.replace("almas-babeu-krohn/", "almas-babeu-krohn/isaw-papers-7-3.xhtml")
-	text = text.replace("benefiel-sprenkle/", "benefiel-sprenkle//isaw-papers-7-4.xhtml")
-	text = text.replace("blackwell-smith/", "blackwell-smith//isaw-papers-7-5.xhtml")
-	text = text.replace("elliott-jones/", "elliott-jones/isaw-papers-7-6.xhtml")
-	text = text.replace("hafford/", "hafford/isaw-papers-7-7.xhtml")
-	text = text.replace("heath/", "heath/isaw-papers-7-8.xhtml")
-	text = text.replace("horne/", "horne/isaw-papers-7-9.xhtml")
-	text = text.replace("kansa/", "kansa/isaw-papers-7-10.xhtml")
-	text = text.replace("lana/", "lana/isaw-papers-7-11.xhtml")
-	text = text.replace("liuzzo/", "liuzzo/isaw-papers-7-12.xhtml")
-	text = text.replace("mackay/", "mackay/isaw-papers-7-13.xhtml")
-	text = text.replace("mcmichael/", "mcmichael/isaw-papers-7-14.xhtml")
-	text = text.replace("meadows-gruber/", "meadows-gruber/isaw-papers-7-15.xhtml")
-	text = text.replace("meyers/", "meyers/isaw-papers-7-16.xhtml")
-	text = text.replace("murray/", "murray/isaw-papers-7-17.xhtml")
-	text = text.replace("nurmikko-fuller/", "nurmikko-fuller/isaw-papers-7-18.xhtml")
-	text = text.replace("pearce-schmitz/", "pearce-schmitz/isaw-papers-7-19.xhtml")
-	text = text.replace("pett/", "pett/isaw-papers-7-20.xhtml")
-	text = text.replace("poehler/", "poehler/isaw-papers-7-21.xhtml")
-	text = text.replace("rabinowitz/", "rabinowitz/isaw-papers-7-22.xhtml")
-	text = text.replace("reinhard/", "reinhard/isaw-papers-7-23.xhtml")
-	text = text.replace("romanello/", "romanello/isaw-papers-7-24.xhtml")
-	text = text.replace("roueche-lawrence-lawrence/", "roueche-lawrence-lawrence/isaw-papers-7-25.xhtml")
-	text = text.replace("seifried/", "seifried/isaw-papers-7-26.xhtml")
-	text = text.replace("simon-barker-desoto-isaksen/", "simon-barker-desoto-isaksen/isaw-papers-7-27.xhtml")
-	text = text.replace("taylor/", "taylor/isaw-papers-7-28.xhtml")
-	text = text.replace("tsonev/", "tsonev/isaw-papers-7-29.xhtml")
-	text = text.replace("vankeer/", "vankeer/isaw-papers-7-30.xhtml")
-	
-with open('7/isaw-papers-7-ereaders.xhtml', 'w') as paper:
-	paper.write(text)
-		
